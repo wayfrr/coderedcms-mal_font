2e9a56898ff70489ecf86f438a118397973842dd
wayfrr <wayfrr@users.noreply.github.com>
2021-08-31 10:12:42 +0530

    Update base.py

--- testproj/settings/base.py
+++ testproj/settings/base.py
@@ -217,8 +217,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'nair.network1@gmail.com'
-EMAIL_HOST_PASSWORD = 'zeblqzegymuwzyzm'
+EMAIL_HOST_USER = 'nxxx.xxxxxxx1@gmail.com'
+EMAIL_HOST_PASSWORD = ' '
 DEFAULT_FROM_EMAIL = "Helpdesk <helpdesk@yourdomain>"
 
 
