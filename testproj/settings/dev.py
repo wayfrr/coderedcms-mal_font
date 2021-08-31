541871121100bd16b20bbaef82e7122c1379fe89
wayfrr <wayfrr@gmail.com>
2021-08-31 10:03:01 +0530

    first commit

---
+++ testproj/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *  # noqa
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'ykdql+e&z2p6n&=@dhu=ezku=7o!nywxb&2$m7xz!r!d8ar76y'
+
+ALLOWED_HOSTS = ['*']
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+WAGTAIL_CACHE = False
+
+try:
+    from .local_settings import *  # noqa
+except ImportError:
+    pass
